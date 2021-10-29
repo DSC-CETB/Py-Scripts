5180b5f07a25e97ce38a9c90bce81792ac01b1c3
Anshul Choudhary <34908313+achoudh5@users.noreply.github.com>
2021-10-29 11:20:25 -0400

    Add files via upload

---
+++ scripts/Remove_duplicates.py
@@ -0,0 +1,10 @@
+# initializing list
+test_list = [1, 3, 5, 6, 3, 5, 6, 1]
+print ("The original list is : " +  str(test_list))
+res = []
+for i in test_list:
+    if i not in res:
+        res.append(i)
+  
+# printing list after removal 
+print ("The list after removing duplicates : " + str(res))
